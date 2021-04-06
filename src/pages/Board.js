e138b468e9e7772d250ab7da87bdf4be5afe92b3
RobertoGallego <groberto@student.42.fr>
2021-04-06 17:04:04 +0200

    tetrominos grip and differents tetrorminos done with rotation and endgame, level ans score almost finish, issue with the top grip axis Y top

--- src/pages/Board.js
+++ src/pages/Board.js
@@ -6,11 +6,15 @@ class Board extends React.Component{
 
 	state = {
 		grid: null,
-		gridHeight: 10,
+		gridHeight: 6,
 		gridWidth: 8,
 		tetromino: null,
+        linesCompletes: 0,
+        level: 1,
+        gameOver: false
 	}
 
+    // https://www.youtube.com/watch?v=361zUVOyRig keys 9
 	// preview next piece jaja
 	componentDidMount() { 
         this.initGame()
@@ -31,8 +35,15 @@ class Board extends React.Component{
                     break;
                 case 'ArrowDown' : this.pieceMovePosDown(1)
                     break;
+                case 'z' : this.rotatePiece("right")
+                break;
+                case 'x' : this.rotatePiece("left")
+                break;
+                default: break;
             }
-        })
+        });
+
+
 	}
 
 
@@ -41,9 +52,22 @@ class Board extends React.Component{
 			grid: this.buildGrid()
         }, () => {
             this.makeTetromino()
+            
+            this.launchTimer()
         })
 	}
 
+
+    gameOver = () => {
+		//mettre fin au jeu
+		clearInterval(this.timer)
+		//set status lost game
+		this.setState({ gameOver: true })
+
+		//debind event
+		// window.removeEventListener("keyup", this.keyupActions)
+		// window.removeEventListener("keydown", this.keydownActions)
+	}
 	//GRID FUNCTIONS
 	buildGrid = () => {
 		let grid = []
@@ -57,28 +81,95 @@ class Board extends React.Component{
 		}
 
         // grid[0][0] = 1;
-		return grid
+		return grid;
+	}
+
+    // level timer
+    
+    launchTimer = () => {
+		this.timer = setInterval(() => {
+			this.pieceMovePosDown(1)
+		}, this.convertLvlToTime())
+	}
+
+    convertLvlToTime = () => { 	
+        if (this.state.level === 1) {
+            return (1000)
+        } else if (this.state.level === 2) {
+            return 500
+        }
+		// let interval = this.baseIntervalTimer - (this.state.level - 1) * 35
+		// return (interval < 100) ? 100 : interval
 	}
 
 	makeTetromino = () => {
         let tetromino = {};
-        tetromino.posX = 0;
+        // tetromino.posX = 0;
         tetromino.posY = 0;
-        tetromino.grid = Tetromino[0];
-        tetromino.mergeData = []
 
+        let indexTetromino = Math.floor(Math.random() * Tetromino.length);
+        tetromino.color= indexTetromino + 1;
+        
+        tetromino.grid = Tetromino[Math.floor(Math.random() * Tetromino.length)];
+        // value 0 cordinate = 0 offset y to -1 init spwn
+        if (tetromino.grid[0][0] === 0) {
+            tetromino.posY--;
+        }
+
+        tetromino.posX = Math.floor((this.state.gridWidth - tetromino.grid[0].length) / 2); // premier piece x
+
+
+        tetromino.mergeData = [];
+
+        // 0 is empty
         // if (tetromino.grid[0][0] === 0) { 
 		// 	tetromino.posY--
 		// }
 
         let resultCordinate = this.tetrominoIsPosition(tetromino);
-
+        console.log(resultCordinate);
         if (resultCordinate !== false) {
             tetromino.mergeData = resultCordinate;
 
-            this.setState({tetromino})
+            this.setState({ tetromino });
+        } else {
+            clearInterval(this.timer);
+
+            this.gameOver()
+            // fingame over
         }
         // console.log(tetromino);
+    }
+
+    mergePieceToGrid = () => {
+        const piece = this.state.tetromino;
+        const gridState = this.state.grid;
+        let level = this.state.level;
+        let levelChanged = false;
+        let linesCompletes = this.state.linesCompletes;
+
+        piece.mergeData.forEach(element => {
+            const [y, x] = element.split("_");
+            gridState[y][x] = this.state.tetromino.color;
+        });
+
+        let { cleanGrid, nbrLineCompleted } = this.cleanGrid(gridState);
+        linesCompletes += nbrLineCompleted
+
+        // new level reset interval
+        if (linesCompletes > 1) {
+            level = 2;
+            clearInterval(this.timer);
+            levelChanged = true;
+        }
+        
+        this.setState({ grid: cleanGrid, piece: null, linesCompletes: linesCompletes, level }, () => {
+            this.makeTetromino();
+
+            if (levelChanged) {
+                this.launchTimer();
+            }
+        });
     }
 
 	tetrominoIsPosition = (tetromino) => {
@@ -121,7 +212,7 @@ class Board extends React.Component{
         if (resultCordinate !== false) {
             tetromino.mergeData = resultCordinate;
             this.setState({ tetromino })
-            console.log("---> ",resultCordinate)
+            // console.log("---> ",resultCordinate)
         }
         // console.log(tetromino);
     }
@@ -140,16 +231,145 @@ class Board extends React.Component{
         if (resultCordinate !== false) {
             tetromino.mergeData = resultCordinate;
             this.setState({ tetromino })
-            console.log("---> ",resultCordinate)
+            // console.log("---> ",resultCordinate)
+        } else {
+            this.mergePieceToGrid();
         }
         // console.log(tetromino);
     }
+
+    rotatePiece = (rotation) => {
+        let tetromino = { ...this.state.tetromino }
+
+        if (tetromino === null) {
+            return false
+        }
+
+        let newGrid = []
+
+        if (rotation === "right") {
+            for (let x = tetromino.grid[0].length - 1; x > -1; x--) {
+                let line = [];
+                for (let y = 0; y < tetromino.grid.length; y++) {
+                    // console.log(y + "_" + x + "  >" + tetromino.grid[y][x]);
+                    line.push(tetromino.grid[y][x])
+                } 
+                newGrid.push(line);
+            }
+        }
+        else if (rotation === "left") {
+            for (let x = 0; x < tetromino.grid[0].length; x++) {
+                let line = [];
+                for (let y = tetromino.grid.length - 1; y > -1; y--) {
+                    // console.log(y + "_" + x + "  >" + tetromino.grid[y][x]);
+                    line.push(tetromino.grid[y][x])
+                } 
+                newGrid.push(line);
+            }
+        }
+        tetromino.grid = newGrid;
+
+        let resultCordinate = this.tetrominoIsPosition(tetromino);
+
+        if (resultCordinate !== false) {
+            tetromino.mergeData = resultCordinate;
+            this.setState({ tetromino })
+            // console.log("---> ",resultCordinate)
+        } else {
+            let isPositionUpdate = false
+            // without rotation wall grid
+            if (tetromino.posX < 0) {
+                tetromino.posX = 0;
+                isPositionUpdate = true;
+            } else if (tetromino.grid[0].length + tetromino.posX  > this.state.gridWidth) {
+				tetromino.posX = this.state.gridWidth - tetromino.grid[0].length
+				isPositionUpdate = true
+			} else if (tetromino.posY < 0) {
+				tetromino.posY = 0
+				isPositionUpdate = true
+			}
+
+			if (isPositionUpdate) { 
+				resultCordinate = this.tetrominoIsPosition(tetromino)
+				if (resultCordinate !== false) {
+					tetromino.mergeData = resultCordinate
+					this.setState({ tetromino })
+				}
+			}
+                // resultCordinate = this.tetrominoIsPosition(tetromino);
+            //     if (resultCordinate !== false) {
+            //         tetromino.mergeData = resultCordinate;
+            //         this.setState({ tetromino });
+            //         // console.log("---> ",resultCordinate)
+            //     } 
+            // } else if (tetromino.grid[0].length + this.state.gridWidth > this.state.gridWidth) {
+            //     tetromino.posX = this.state.gridWidth - tetromino.grid[0].length;
+            //     resultCordinate = this.tetrominoIsPosition(tetromino);
+            //     if (resultCordinate !== false) {
+            //         tetromino.mergeData = resultCordinate;
+            //         this.setState({ tetromino });
+            //     }
+            // } else if () {
+            //     tetromino.grid.length + 
+            // }
+
+        }
+
+    }
+
+
+    cleanGrid = (grid) => {
+
+		let cleanGrid = []
+		let nbrLineCompleted = 0
+
+		for (let y = 0; y < this.state.gridHeight; y++) {
+
+			let lineCompleted = true
+			for (let x = 0; x < this.state.gridWidth; x++) {
+				if (grid[y][x] === 0) { 
+					lineCompleted = false
+				}
+			}
+
+			if (lineCompleted === false) {
+				cleanGrid.push(grid[y])
+			}
+
+		}
+
+		nbrLineCompleted = this.state.gridHeight - cleanGrid.length
+
+		for (let i = 0; i < nbrLineCompleted; i++) { 
+			cleanGrid.unshift( this.makeCleanLine(this.state.gridWidth) )
+		}
+
+        // console.log(cleanGrid);
+
+		return { cleanGrid, nbrLineCompleted }
+
+	}
+
+    makeCleanLine(width) { 
+		let line = []
+		for (let x = 0; x < width; x++) {
+			line.push(0)
+		}
+		return line
+	}
 
 	render() { 
 		return (
             <>
                 <div className="game">
-                    <div className="game__pan bg-success p-3">
+                    <div className="game__pan bg-success p-3" style={{display: "flex", flexDirection: "column"}}>
+                        <p className={"score"}>score: { this.state.linesCompletes }</p>
+                        <p className={"level"}>level: { this.state.level }</p>
+                        { this.state.gameOver ?
+                            <p style={{color: "red", position: "absolute", top: 230, left: 45, fontSize: 30}}>GAME OVER</p>
+                        :
+                            ""
+                        }
                         { this.state.grid !== null && 
                             <Grid 
                                 grid={this.state.grid} 
--- src/pages/Grid.js
+++ src/pages/Grid.js
@@ -15,20 +15,29 @@ const Grid = ({ grid, tetromino }) => {
                         return line.map(
                             (col, x) => {
 
-                                return <span key={x + "_" + y} className={
-                                    ((x === 0) ? "first" : "") + " " + 
-                                    ((tetromino !== null && tetromino.mergeData.indexOf(y + "_" + x) !== -1) ? 'color' : "")
-                                }>
-                                    { 
-                                    (tetromino !== null) ?
-                                        (tetromino.mergeData.indexOf(y + "_" + x) !== -1) ? 
-                                            "1" 
-                                        : 
-                                            grid[y][x] 
-                                    :
-                                        grid[y][x] 
+                                let tetrominosSetting = [];
+                                let value = 0;
+
+                                if (x === 0) {
+                                    tetrominosSetting.push("first");
+                                }
+                                if (tetromino !== null) {
+                                    if (tetromino.mergeData.indexOf(y + "_" + x) !== -1) {
+                                        tetrominosSetting.push("color");
+                                        // tetromino.mergeData[0].split
+                                        value = tetromino.color
                                     }
-                                </span>
+                                }
+                                if (grid[y][x] > 0) {
+                                    tetrominosSetting.push("color");
+                                    value = grid[y][x]
+                                }
+
+                                return (
+                                    <span key={x + "_" + y} className={tetrominosSetting.join(" ")}>
+                                        { value }
+                                    </span>
+                                )
                             }
                         )
                     }
