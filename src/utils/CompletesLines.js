74faac34970abeb894d281d2d7323502224fcbad
Thyrse <tfourgeux42@gmail.com>
2021-04-28 14:03:21 +0200

    Fix game over issue

--- src/utils/CompletesLines.js
+++ src/utils/CompletesLines.js
@@ -1,53 +1,52 @@
 // import React, { useState } from "react";
 
-function cleaningLine(width) { 
-    let line = []
-    let x = 0;
+function cleaningLine(width) {
+  let line = [];
+  let x = 0;
 
-    // console.log("line", line)
+  // console.log("line", line)
 
-    while(x < width) {
-        // if (grid.length >= -10) {
-        //     line.push(1)
-        // } else {
-            line.push(0);
-        // }
-        x++;
-    }
-    return line;
+  while (x < width) {
+    // if (grid.length >= -10) {
+    //     line.push(1)
+    // } else {
+    line.push(0);
+    // }
+    x++;
+  }
+  return line;
 }
 
 const cleanGrids = (grid, gridHeight, gridWidth) => {
-    let cleanGrid = []
-    let numberLinesReady = 0
-    let y = 0;
-    
-    while (y < gridHeight) {
-        let lineCompleted = true;
-        let x = 0;
-        while (x < gridWidth) {
-            if (grid[y][x] === 0) { 
-                lineCompleted = false;
-            }
-            x++;
-        }
-        if (lineCompleted === false) {
-            // console.log("number", grid[y]);
-            cleanGrid.push(grid[y]);
-        }      
-        y++;  
+  let cleanGrid = [];
+  let numberLinesReady = 0;
+  let y = 0;
+
+  while (y < gridHeight) {
+    let lineCompleted = true;
+    let x = 0;
+    while (x < gridWidth) {
+      if (grid[y][x] === 0) {
+        lineCompleted = false;
+      }
+      x++;
     }
-
-    numberLinesReady = gridHeight - cleanGrid.length
-    let z  = 0;
-
-    while(z < numberLinesReady) {         
-        cleanGrid.unshift(cleaningLine(grid, gridWidth))
-        z++;
+    if (lineCompleted === false) {
+      // console.log("number", grid[y]);
+      cleanGrid.push(grid[y]);
     }
+    y++;
+  }
 
+  numberLinesReady = gridHeight - cleanGrid.length;
+  let z = 0;
 
-    return { cleanGrid, numberLinesReady }
-}
+  while (z < numberLinesReady) {
+    cleanGrid.unshift(cleaningLine(gridWidth));
+    z++;
+  }
+
+  return { cleanGrid, numberLinesReady };
+};
 
 export default cleanGrids;
